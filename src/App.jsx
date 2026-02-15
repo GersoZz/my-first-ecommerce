609b98348627704765efec4f94a2a011281aef02
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-02-14 20:33:17 -0500

    feat: refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,39 @@ import './App.css'
 import ProductCard from './components/ProductCard'
 
 function App() {
+  const productsData = [
+    {
+      title: 'Zapatillas Runing',
+      price: 120,
+      description: 'Las mejores zapatillas',
+      imageUrl: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff',
+    },
+    {
+      title: 'Reloj de Tiempo',
+      price: 200,
+      description: 'El mejor reloj de Tiempo',
+      imageUrl: 'https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c',
+    },
+    {
+      title: 'Mouse RGB',
+      price: 100,
+      description: 'El mejor mouse RGB',
+    },
+  ]
+
   return (
     <>
-      <ProductCard
-        title="Zapatillas Runing"
-        price="120"
-        description="Las mejores zapatillas"
-        imageUrl="https://images.unsplash.com/photo-1542291026-7eec264c27ff"
-      />
-
-      <ProductCard
-        title="Reloj de Tiempo"
-        price="200"
-        description="El mejor reloj de Tiempo"
-        imageUrl="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c"
-      />
-
-      <ProductCard title="Mouse RGB" price="100" description="El mejor mouse RGB" />
+      <div className="card-list">
+        {productsData.map((product, index) => (
+          <ProductCard
+            key={index}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+            imageUrl={product.imageUrl}
+          />
+        ))}
+      </div>
     </>
   )
 }
