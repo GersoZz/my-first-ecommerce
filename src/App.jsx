87fa0b762af25ea44b9ca5a744e93b6f3254db16
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-02-26 22:25:34 -0500

    feat: add ErrorMessage component for error handling in product fetching

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,28 @@ import Container from './components/Container'
 import { productsAdapter } from './adapters/products.adapter'
 import Loader from './components/Loader'
 import useFetch from './hooks/useFetch'
+import ErrorMessage from './components/ErrorMessage'
 
 function App() {
   const [showPromo, setShowPromo] = useState(true)
 
-  const { data: productsRawData, isLoading: isLoadingProducts } = useFetch(
-    'https://api.escuelajs.co/api/v1/products?offset=0&limit=6',
-  )
+  const {
+    data: productsRawData,
+    isLoading: isLoadingProducts,
+    error: errorProducts,
+  } = useFetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=6')
 
-  const { data: suggestedRawData, isLoading: isLoadingSuggested } = useFetch(
-    'https://api.escuelajs.co/api/v1/products?offset=6&limit=2',
-  )
+  const {
+    data: suggestedRawData,
+    isLoading: isLoadingSuggested,
+    error: errorSuggested,
+  } = useFetch('https://api.escuelajs.co/api/v1/products?offset=6&limit=2')
 
-  const { data: snacksRawData, isLoading: isLoadingSnacks } = useFetch(
-    'https://api.escuelajs.co/api/v1/categories/19/products?offset=0&limit=6',
-  )
+  const {
+    data: snacksRawData,
+    isLoading: isLoadingSnacks,
+    error: errorSnacks,
+  } = useFetch('https://api.escuelajs.co/api/v1/categories/19/products?offset=0&limit=6')
 
   const productsData = productsRawData ? productsRawData.map(productsAdapter) : []
   const productsSuggestedData = suggestedRawData ? suggestedRawData.map(productsAdapter) : []
@@ -35,20 +42,31 @@ function App() {
     <>
       <Header />
       {showPromo && <PromoBanner onClose={onClose} />}
-      <Container title="Productos disponibles">
-        <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Estos son los productos que puedes comprar</p>
-        {isLoadingProducts ? <Loader /> : <ProductList productsData={productsData} />}
-      </Container>
 
-      <Container title="Productos sugeridos">
-        <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de productos</p>
-        {isLoadingSuggested ? <Loader /> : <ProductList productsData={productsSuggestedData} />}
-      </Container>
+      {errorProducts && <ErrorMessage message={'No se pudo cargar los productos'} />}
+      {errorSuggested && <ErrorMessage message={'No se pudo cargar los productos sugeridos'} />}
+      {errorSnacks && <ErrorMessage message={'No se pudo cargar los snacks'} />}
 
-      <Container title="Productos snacks">
-        <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de snacks</p>
-        {isLoadingSnacks ? <Loader /> : <ProductList productsData={productsSnacksData} />}
-      </Container>
+      {!errorProducts && (
+        <Container title="Productos disponibles">
+          <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Estos son los productos que puedes comprar</p>
+          {isLoadingProducts ? <Loader /> : <ProductList productsData={productsData} />}
+        </Container>
+      )}
+
+      {!errorSuggested && (
+        <Container title="Productos sugeridos">
+          <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de productos</p>
+          {isLoadingSuggested ? <Loader /> : <ProductList productsData={productsSuggestedData} />}
+        </Container>
+      )}
+
+      {!errorSnacks && (
+        <Container title="Productos snacks">
+          <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de snacks</p>
+          {isLoadingSnacks ? <Loader /> : <ProductList productsData={productsSnacksData} />}
+        </Container>
+      )}
     </>
   )
 }
