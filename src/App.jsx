2f711ce24db4befc4f4a1e7b84f06e3b7c7734fc
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-02-26 21:30:17 -0500

    feat: create productsAdapter for product data transformation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PromoBanner from './components/PromoBanner'
 import Header from './components/Header'
 import ProductList from './components/ProductList'
 import Container from './components/Container'
+import { productsAdapter } from './adapters/products.adapter'
 
 function App() {
   const [showPromo, setShowPromo] = useState(true)
@@ -17,25 +18,7 @@ function App() {
     fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=6')
       .then((response) => response.json())
       .then((data) => {
-        /* Ctrl Alt L */
-        console.log('🚀 ~ App ~ data:', data)
-
-        const productsAdapteds = data.map((product) => {
-          // Desestructuracion de Javascript
-          const { id, title, price, description, images } = product
-
-          // Asignando en base al nombre
-          const productAdapted = {
-            id,
-            title,
-            price,
-            description,
-            imageUrl: images[0],
-          }
-
-          return productAdapted
-        })
-
+        const productsAdapteds = data.map(productsAdapter)
         setProductsData(productsAdapteds)
       })
       .catch((error) => console.error(error))
@@ -44,23 +27,7 @@ function App() {
     fetch('https://api.escuelajs.co/api/v1/products?offset=6&limit=2')
       .then((response) => response.json())
       .then((data) => {
-        /* Ctrl Alt L */
-        console.log('🚀 ~ App ~ data:', data)
-
-        const productsAdapteds = data.map((product) => {
-          const { id, title, price, description, images } = product
-
-          const productAdapted = {
-            id,
-            title,
-            price,
-            description,
-            imageUrl: images[0],
-          }
-
-          return productAdapted
-        })
-
+        const productsAdapteds = data.map(productsAdapter)
         setProductsSuggestedData(productsAdapteds)
       })
       .catch((error) => console.error(error))
@@ -69,23 +36,7 @@ function App() {
     fetch('https://api.escuelajs.co/api/v1/categories/19/products?offset=0&limit=6')
       .then((response) => response.json())
       .then((data) => {
-        /* Ctrl Alt L */
-        console.log('🚀 ~ App ~ data:', data)
-
-        const productsAdapteds = data.map((product) => {
-          const { id, title, price, description, images } = product
-
-          const productAdapted = {
-            id,
-            title,
-            price,
-            description,
-            imageUrl: images[0],
-          }
-
-          return productAdapted
-        })
-
+        const productsAdapteds = data.map(productsAdapter)
         setProductsSnacksData(productsAdapteds)
       })
       .catch((error) => console.error(error))
