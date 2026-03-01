eafcd1cfa45d8d120aecefd9c9f2181a01b7d620
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-02-28 20:51:49 -0500

    feat: implement routerConfig and refactoring AppRouter

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
-import { AboutPage, AddProductPage, CartPage, HomePage } from './pages'
+import routes from './routerConfig'
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/add-product" element={<AddProductPage />} />
-      <Route path="/cart" element={<CartPage />} />
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
     </Routes>
   )
 }
